972b75992d8af34cfef0b7f66183236a4c3fdc4e
Gustavo Romero <gustavo19romero@gmail.com>
2026-04-01 15:00:22 -0300

    Chore: Acrescentando as Características do vinho

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,8 @@ let email = prompt('Insira seu email: ');
 let idade = prompt('Insira sua idade: ');
 alert('Cadastro realizado! Veja os detalhes no console.');
 console.log(`Seu cadastro foi completo ${nome}, ${email}, ${idade}`);
+
+
+let tipo = prompt('Insira o tipo de vinho desejado: ');
+let vinho = prompt('Insira o nome do seu vinho: ');
+let safra = prompt('Insira a safra desejada de seu vinho: ');
