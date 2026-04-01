dba51225a88e0d8827b5e706a47a329d25809779
Gustavo Romero <gustavo19romero@gmail.com>
2026-04-01 15:07:46 -0300

    Chore: Acrescentando console.log

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,6 @@ console.log(`Seu cadastro foi completo ${nome}, ${email}, ${idade}`);
 let tipo = prompt('Insira o tipo de vinho desejado: ');
 let vinho = prompt('Insira o nome do seu vinho: ');
 let safra = prompt('Insira a safra desejada de seu vinho: ');
+alert('A seguir, veja os detalhes do vinho no console.');
+console.log(`O tipo escolhido de vinho foi ${tipo}, O vinho escolhido foi o ${vinho} e sua safra escolhida foi ${safra}`);
+console.log(`A quantidade desse vinho em estoque é de 5 unidades`);
